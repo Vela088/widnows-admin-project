93f08a92873d819f216f07b0fafb0ffee4af7c4c
Vela088 <adrian.vela@alumne.escolabalmes.com>
2025-12-11 17:09:52 +0100

    script con modifiacion de fecha

--- Check-Disk.ps1
+++ Check-Disk.ps1
@@ -1,1 +1,3 @@
+$fecha = Get-Date
+Write-Output "analisis realizado el :$fecha"
 Get-Volume | Select-object DriveLetter, FileSystem, SizeRemaning, SizeWrite-Output "Revisión del disco completada correctamente."
